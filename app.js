3ab93cefacb800088f39154bc39904af66fc4171
Raksheet Bhat <raksheetbhat@MacBook-Air.lan>
2022-09-06 18:50:06 -0500

    restructure code

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(fileUpload({
 }));
 
 //add other middleware
+app.use(express.static('public'));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -29,10 +30,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/functions', functionsRouter);
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
